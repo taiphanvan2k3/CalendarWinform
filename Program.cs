796cf1af9d354d083b99eeb3f71681f0bfd35bc7
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-02-17 23:16:46 +0700

    second commit

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace CalendarWinform
         [STAThread]
         static void Main()
         {
+            //Test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
